a548076a1a17f10520987597101329886a59c232
tuansdf <103062682+tuansdf@users.noreply.github.com>
2022-11-26 19:51:47 +0700

    encode uri

--- src/app.svelte
+++ src/app.svelte
@@ -18,33 +18,32 @@
   };
 
   onMount(() => {
-    // a: slug, z: redirect
     const params = new URLSearchParams(window.location.search);
 
     // get url or base64
-    const slug = params.get("a") || window.location.pathname.slice(1);
-    const redirect = !params.get("z");
+    const slug =
+      window.location.pathname.slice(1) || window.location.search.slice(1);
+    const redirect = params.get("redirect") !== "0";
 
     // if it is a url, encode
     if (isValidUrl(slug)) {
       const encoded = Base64.encode(slug, true);
       text = window.location.origin + "/" + encoded;
+      return;
     }
     // if it is a base64, decode
     else if (Base64.isValid(slug)) {
-      const decoded = Base64.decode(slug);
+      const decoded = encodeURI(Base64.decode(slug));
       if (isValidUrl(decoded)) {
         if (redirect) {
           window.location.href = decoded;
         } else {
           text = decoded;
         }
-      } else {
-        error = ERROR;
+        return;
       }
-    } else {
-      error = ERROR;
     }
+    error = ERROR;
   });
 </script>
 
