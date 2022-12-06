78fdc1018f0b9298f1205aeb396e2818ef7ee65c
tuansdf <103062682+tuansdf@users.noreply.github.com>
2022-12-06 20:00:34 +0700

    change style

--- src/app.svelte
+++ src/app.svelte
@@ -49,27 +49,33 @@
   });
 </script>
 
-{#if index}
-  <h2>Bookmarklet</h2>
-  <p>
-    If you want to easily create a URL redirect, drag<br />
-    <a
-      href={`javascript:(function()%7Bconst%20a=%22${window.origin}/?%22+window.location;window.open(a,%22_blank%22)%7D)();`}
-      >this</a
-    ><br />
-    to the bookmark toolbar
-  </p>
+<main>
+  {#if index}
+    <h2>Bookmarklet</h2>
+    <p>
+      If you want to easily create a URL redirect, drag<br />
+      <a
+        href={`javascript:(function()%7Bconst%20a=%22${window.origin}/?%22+window.location;window.open(a,%22_blank%22)%7D)();`}
+        >this</a
+      ><br />
+      to the bookmark toolbar
+    </p>
 
-  <h2>Create</h2>
-  <input type="text" bind:value={linkInput} placeholder="https://example.com" />
-{/if}
-
-{#if isError}
-  <p>Something is wrong...</p>
-{:else if text}
-  <a href={text}>{text}</a><br />
-  <button on:click={onCopy}>Copy to clipboard</button>
-  {#if isCopied}
-    <span>Copied</span>
+    <h2>Create</h2>
+    <input
+      type="text"
+      bind:value={linkInput}
+      placeholder="https://example.com"
+    />
   {/if}
-{/if}
+
+  {#if isError}
+    <p>Something is wrong...</p>
+  {:else if text}
+    <a href={text}>{text}</a><br />
+    <button on:click={onCopy}>Copy to clipboard</button>
+    {#if isCopied}
+      <span>Copied</span>
+    {/if}
+  {/if}
+</main>
